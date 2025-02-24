6d7f6562c7643ab0671fc3a4650318721b450a05
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-02-23 20:45:53 -0500

    install axios

--- Proyecto/backend/src/payment/payment.module.ts
+++ Proyecto/backend/src/payment/payment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [PaymentService],
-  controllers: [PaymentController]
+  controllers: [PaymentController],
+  imports: [ConfigModule],
 })
 export class PaymentModule {}
