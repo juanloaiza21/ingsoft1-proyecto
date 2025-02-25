3270f76d9ced7ac1b606d4e2e7d46077151dc98b
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-02-25 18:19:13 -0500

    Paypal on backend its like the hell

--- Proyecto/backend/src/payment/payment.controller.ts
+++ Proyecto/backend/src/payment/payment.controller.ts
@@ -1,16 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { GenerateBuyDTO } from './dto/genBuy.dto';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paypalService: PaymentService) {}
-
-  /**
-   * @todo implement JWT for identification
-   */
-  @Post()
-  async getAuth(@Body() payment: GenerateBuyDTO) {
-    return await this.paypalService.genBuy(payment);
-  }
 }
