3c74081eeab3a0e46126dd7dadc9ef0f6f54144f
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-03-03 00:56:55 -0500

    Profile integrated

--- Proyecto/frontend/app/config/config.ts
+++ Proyecto/frontend/app/config/config.ts
@@ -16,7 +16,7 @@ export const ConfigVariables = {
         url: `${url}/users/`,
       },
       update: {
-        method: 'PUT',
+        method: 'PATCH',
         url: `${url}/users/`,
       },
       getOne: {
